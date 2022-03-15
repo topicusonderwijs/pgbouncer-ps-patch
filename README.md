95dd44c24274cbd4d28cd7ca167a165b8a77d6a0
Sverre Boschman <1142569+sboschman@users.noreply.github.com>
2022-03-15 13:20:41 +0100

    add install script

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # PgBouncer with prepared statement support
+
+# Getting Started
+
+**Install**  
+Download and install pgbouncer-ps by running the following commands (RHEL/CentOS):
+```
+# install required packages - see https://github.com/pgbouncer/pgbouncer#building
+sudo yum install libevent-devel openssl-devel python-devel libtool git patch make -y
+
+# download the latest tested pgbouncer distribution - 1.14
+git clone https://github.com/pgbouncer/pgbouncer.git --branch "pgbouncer_1_14_0"
+
+# download pgbouncer-ps extensions
+git clone git@github.com:topicus-education-ops/pgbouncer-ps-patch.git
+
+# merge pgbouncer-ps extensions into pgbouncer code
+cd pgbouncer-ps-patch
+./install-pgbouncer-ps-patch.sh ../pgbouncer
+
+# build and install
+cd ../pgbouncer
+git submodule init
+git submodule update
+./autogen.sh
+./configure ...
+make
+sudo make install
+```
+
+**Configure**  
+Create a configuration file, using `./etc/pgbouncer.ini` as a starting point.
+
+By default, prepared statement support is not enabled. To enable it just add the following to your pgbouncer-ps configuration:
+```
+disable_prepared_statement_support = 1
+```
+
+The number of prepared statements kept active on a single backend connection is defined by the following configuration:
+```
+prepared_statement_cache_queries = 100
+```
+Note: keep in mind that this will increase the memory footprint of each client connection on your PostgreSQL server.
---
+++ install-pgbouncer-ps-patch.sh
@@ -0,0 +1,61 @@
+USAGE="Usage: $0 <pgbouncer dir>"
+usage() {
+   echo $USAGE
+   exit 1
+}
+
+PGDIR=$1
+[ -z "$PGDIR" ] && usage
+
+PATCHDIR=$(pwd)
+patchstatus=0
+
+# Patch each modified file
+MERGEFILES="\
+  Makefile\
+  etc/pgbouncer.ini\
+  src/client.c\
+  src/main.c\
+  src/objects.c\
+  src/sbuf.c\
+  src/server.c\
+  src/stats.c\
+  include/bouncer.h\
+  include/sbuf.h\
+  "
+for file in $MERGEFILES
+do
+   echo Merging pgbouncer-ps changes to: $PGDIR/$file
+   patch -d $PGDIR -f -b -p1 < $PATCHDIR/$file.diff || patchstatus=1
+done
+
+# copy pgbouncer-ps source files
+mkdir -p $PGDIR/images
+NEWFILES="\
+  include/messages.h\
+  include/ps.h\
+  include/common/uthash.h\
+  src/messages.c\
+  src/ps.c\
+  "
+echo -n "copying pgbouncer-ps files: "
+for file in $NEWFILES
+do
+   echo -n "$file "
+   cp $PATCHDIR/$file $PGDIR/$file || patchstatus=1
+done
+echo
+
+if [ $patchstatus -eq 1 ]; then
+   echo "Failures encountered during merge of pgbouncer-ps with pgbouncer."
+   echo "See error messages above."
+   echo "Possible causes: "
+   echo "   pgbouncer-ps-patch already installed in target directory?"
+   echo "   new version of pgbouncer with changed source files that can't be patched?"
+   echo "      - last tested with pgbouncer v1.14 (June 2020)"
+   echo "      - Try getting pgbouncer with: git clone https://github.com/pgbouncer/pgbouncer.git --branch \"pgbouncer_1_14_0\""
+   echo "Status: pgbouncer-ps-patch merge FAILED"
+else
+   echo "Status: pgbouncer-ps-patch merge SUCEEDED"
+fi
+exit $patchstatus
