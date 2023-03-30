59cbb5632f782b32d5821d29d12301528f64d151
Sverre Boschman <1142569+sboschman@users.noreply.github.com>
2023-03-30 17:14:41 +0200

    Revert "add missing files for client/server show commands for prepared statement parse/bind stats"
    This reverts commit 5cef3fc6c80b44579e0dc5b3255e97850b811f39.

--- generate-diffs.sh
+++ generate-diffs.sh
@@ -11,15 +11,14 @@ DST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MERGEFILES="\
   Makefile\
   etc/pgbouncer.ini\
-  include/bouncer.h\
-  include/sbuf.h\
-  src/admin.c\
   src/client.c\
   src/main.c\
   src/objects.c\
   src/sbuf.c\
   src/server.c\
   src/stats.c\
+  include/bouncer.h\
+  include/sbuf.h\
   "
 
 NEWFILES="\
